2e6005690b7ebda73d5b113151430e3a16032858
Carlos <carlos.emmanuel.236@gmail.com>
2023-11-06 18:24:22 -0300

    Adicionando a pagina de home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,59 @@
 import Menu from "../../components/Menu";
 import classes from "./Home.module.css";
+import Highlight, {MovieProps} from "../../components/Highlight/Highlight.tsx";
+import {useEffect, useState} from "react";
 
-function Home(){
-    return(
-        <div className={classes.background}>
-            <Menu />
+const IMG_URL: string = import.meta.env.VITE_IMG;
+
+function Home() {
+    const [movie, setMovie] = useState({} as MovieProps);
+
+    useEffect(() => {
+        requestMovie();
+    }, []);
+
+    async function requestMovie() {
+        try {
+            const response = await fetch('https://api.themoviedb.org/3/movie/550?language=pt-Br', {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOGQyZTQ3NTkwMTMyOTE1NjZmYzc4ZDhiN2MxNjg2YSIsInN1YiI6IjY1NDJmM2E2ZWQyYWMyMDExZTRiZGZjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0iESygWdEhVErVrKxGLP7nbBkIn8Y52CAsqFVYb9lVs'
+                },
+            });
+
+            const resData = await response.json();
+            setMovie(resData);
+        } catch (e: any) {
+            throw new Error(e.statusText);
+        }
+    }
+
+    return (
+        <div className={classes.o}>
+            <div
+                style={{backgroundImage: `url(${IMG_URL}${movie.backdrop_path})`}}
+                className={classes.background}
+            >
+                <Menu/>
+                <Highlight
+                    movie={movie}
+                    type={'duration'}
+                    descriptionPosition={'top'}
+                    hasGenre
+                    hasWatchNow
+                    hasMoreInfo
+                    hasAdd
+                    hasFav
+                />
+            </div>
+
+            <div className={classes.carousel}>
+                <Menu/>
+                <Menu/>
+                <Menu/>
+                <Menu/>
+            </div>
         </div>
     )
 }
