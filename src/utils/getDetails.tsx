1a7684be3a92783032af016e70da1d05a65cfbf2
Carlos <carlos.emmanuel.236@gmail.com>
2023-11-06 18:16:22 -0300

    Adicionando funcoes auxiliares

---
+++ src/utils/getDetails.tsx
@@ -0,0 +1,58 @@
+import {MovieGenres} from "../components/Highlight/Highlight.tsx";
+
+export function getGenres(genres: MovieGenres[]) {
+    if (!genres) {
+        return "";
+    }
+
+    const genresArray = genres.map((genre) => {
+        return genre.name;
+    });
+
+    if (genres.length === 1) {
+        return genresArray[0];
+    }
+
+    const lastGenre = genresArray.pop();
+    return genresArray.join(', ') + ' & ' + lastGenre;
+}
+
+export function getReleaseYear(date: string) {
+    if (!date) {
+        return "";
+    }
+
+    return date.split('-')[0];
+}
+
+export function getDuration(duration: number) {
+    const hours = Math.floor(duration / 60);
+    const minutes = duration % 60;
+
+    return `${hours} h ${minutes} min`;
+}
+
+export function getNumberOfSeasons(numberOfSeasons: number) {
+    if (!numberOfSeasons) {
+        return "";
+    }
+
+    return `${numberOfSeasons} Temporada${numberOfSeasons > 1 ? 's' : ''}`;
+}
+
+export function getNumberOfMovies(numberOfMovies: number) {
+    if (!numberOfMovies) {
+        return "";
+    }
+
+    return `${numberOfMovies} Filme${numberOfMovies > 1 ? 's' : ''}`;
+}
+
+export function getRating(rating: number) {
+    if (!rating) {
+        return "";
+    }
+
+    return `${rating} `;
+}
+
