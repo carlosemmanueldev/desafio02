29e2334e898523e2db60298be5a0724a6b2b144d
TayaneCibely <tayanerodrigues2001@gmail.com>
2023-11-07 10:01:20 -0300

    restaurando commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./pages/Login";
-import Home from "./pages/Home/Home.tsx";
+import Home from "./pages/Home";
 import Actors from "./pages/Actors";
 import TvShows from "./pages/TvShows";
-import Movie from "./pages/Movies/Movie.tsx";
+import Movies from "./pages/Movies";
 import Collections from "./pages/Collections";
-import Search from "./pages/Search/Search.tsx";
+import Search from "./pages/Search";
 import MyList from "./pages/MyList";
 import PrivateRoute from "./private-routes/PrivateRoute.tsx";
 import Approved from "./pages/Login/approved.tsx";
 
-
 function App() {
-  return (
-          <BrowserRouter>
+    return (
+        <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/approved" element={<Approved />} />
                 <Route path="/" element={<PrivateRoute component={Home} />} />
                 <Route path="/actors" element={<PrivateRoute component={Actors} />} />
                 <Route path="/tvshows" element={<PrivateRoute component={TvShows} />} />
-                <Route path="/movies" element={<PrivateRoute component={Movie} />} />
+                <Route path="/movies" element={<PrivateRoute component={Movies} />} />
                 <Route path="/collections" element={<PrivateRoute component={Collections} />} />
                 <Route path="/search" element={<PrivateRoute component={Search} />} />
                 <Route path="/mylist" element={<PrivateRoute component={MyList} />} />
