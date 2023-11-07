8cca7bf97bf8f20cd68da314f384cc4deef07e80
Carlos <carlos.emmanuel.236@gmail.com>
2023-11-07 11:45:09 -0300

    Retirando o strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -8,8 +7,6 @@ import store from "./store";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
+        <App/>
     </Provider>
 )
